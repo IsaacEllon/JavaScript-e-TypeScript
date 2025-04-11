2884d62b7b76283d0ec7bab9f9ff718158ee32bc
Isaac Ellon <155269775+IsaacEllon@users.noreply.github.com>
2025-04-11 16:59:00 -0300

    1

---
+++ 6-operadores_aritmeticos_atribuicao_incremeto/operadores.js
@@ -0,0 +1,45 @@
+// Aritmético
+// +  adição / Concatenação
+// -  Subtração
+// *  Multiplicação
+// /  Divisão
+// ** Potenciação
+// &  Resto da divisão
+
+// Precedência
+// ()
+// **
+// * , / , %
+// + , - 
+
+const num1 = 5;
+const num2 = 2;
+const num3 = 10;
+const resultado = (num1 + num2) * num3;
+console.log(resultado);
+
+// Incremento
+// ++
+let contador = 1;
+contador++;  // ou ++contador;
+contador++;  // ou ++contador;
+console.log(contador);
+
+// Decremento
+// --
+let contar = 10;
+contar--;  // ou --contar;
+contar--;  // ou --contar;
+console.log(contar);
+
+// Atribuição
+// +=
+// -=
+// *=
+// /=
+// **=
+let atribuicao = 2;
+atribuicao *= 2;
+atribuicao *= 2;
+console.log(atribuicao);
+

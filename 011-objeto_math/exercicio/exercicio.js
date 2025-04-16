6f3afa6ee9b1c6c56a52eab22328bf5ef2267820
Isaac Ellon <155269775+IsaacEllon@users.noreply.github.com>
2025-04-16 17:54:55 -0300

    1

--- 011-objeto_math/exercicio/exercicio.js
+++ 011-objeto_math/exercicio/exercicio.js
@@ -1,8 +1,19 @@
-const input = prompt('Digite um número:');
+const numero = Number(prompt('Digite um número:'));
 
-document.body.innerHTML = `Raiz quadrada: ${input ** 0.5}<br />`;
-document.body.innerHTML += `${input} é inteiro: ${Number.isInteger(input)}<br />`;
-document.body.innerHTML += `É NaN: ${Number.isNaN(input)}<br />`;
-document.body.innerHTML += `Arredondado para baixo: ${Math.floor(input)}<br />`;
-document.body.innerHTML +=`Arredondado para cima: ${Math.ceil(input)}<br />`;
-document.body.innerHTML += `Com duas casas decimais: ${input.toFixed(2)}<br />`;
+const numerotitulo = document.getElementById('numero-titulo');
+const texto = document.getElementById('texto');
+
+numerotitulo.innerHTML = numero;
+texto.innerHTML = `<p>Raiz quadrada: ${numero ** 0.5} </p>`;
+texto.innerHTML += `${numero} é inteiro: ${(Number.isInteger(numero))}<br /><br />`;
+texto.innerHTML += `É NaN: ${Number.isNaN(numero)}<br /><br />`;
+texto.innerHTML += `Arredondado para baixo: ${Math.floor(numero)}<br /><br />`;
+texto.innerHTML += `Arredondado para cima: ${Math.ceil(numero)}<br /><br />`;
+texto.innerHTML += `Com duas casas decimais: ${numero.toFixed(2)}<br /><br />`;
+
+// document.body.innerHTML = `Raiz quadrada: ${input ** 0.5}<br /><br />`;
+// document.body.innerHTML += `${input} é inteiro: ${parseFloat(Number.isInteger(input))}<br /><br />`;
+// document.body.innerHTML += `É NaN: ${Number.isNaN(input)}<br /><br />`;
+// document.body.innerHTML += `Arredondado para baixo: ${Math.floor(input)}<br /><br />`;
+// document.body.innerHTML += `Arredondado para cima: ${Math.ceil(input)}<br /><br />`;
+// document.body.innerHTML += `Com duas casas decimais: ${parseFloat(input.toFixed(2))}<br /><br />`;

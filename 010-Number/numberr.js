1a71b71b7223352046db04a660e6131f79b62bf4
Isaac Ellon <155269775+IsaacEllon@users.noreply.github.com>
2025-04-16 09:17:04 -0300

    1

--- 010-Number/numberr.js
+++ 010-Number/numberr.js
@@ -6,7 +6,7 @@ let num4 = 0.1;
 
 num3 += num4;  // 0.8;
 num3 += num4;  // 0.9;
-//num3 += num4;  // 1.0;
+num3 += num4;  // 1.0;
 num3 = parseFloat(num3.toFixed(2));
 console.log(num3);
 console.log(Number.isInteger(num3))

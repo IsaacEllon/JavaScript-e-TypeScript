2c12b9aa4462f70bbe67ee7621091e93af6df724
Isaac Ellon <155269775+IsaacEllon@users.noreply.github.com>
2025-04-17 14:00:57 -0300

    1

---
+++ 012-arrays-basico/indexararay.js
@@ -0,0 +1,24 @@
+// Array é meio que uma lista de coisas
+// Array é indexavel por elemento
+//                0        1       2
+const alunos = ['Luiz', 'Maria', 'João'];
+// alunos[0] = 'Eduardo'; // substitui o elemento
+// alunos[3] = 'Luiza';  // Adicionou um elemento por não haver nenhum elemento no index 3.
+
+// alunos[alunos.length] = 'Luiza';  // Adiciona um novo elemento
+// alunos[alunos.length] = 'Fábio';  // Adiciona um novo elemento
+// alunos[alunos.length] = 'Luana';  // Adiciona um novo elemento
+
+// alunos.push('Ótavio');  // Função que adiciona um elemento no final
+// alunos.push('Gabriel'); // Função que adiciona um elemento no final
+
+alunos.unshift('Ellon');  // Função que adiciona um elemento no começo
+alunos.unshift('Isaac');  // Função que adiciona um elemento no começo
+
+// const removido = alunos.pop(); // Remove elemento do final 
+// const removido = alunos.shift(); // Remove do começo
+// console.log(removido)
+
+delete.alunos[1];
+
+console.log(alunos);

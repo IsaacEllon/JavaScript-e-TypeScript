6f68e0fc2744f34e40dd3ffa5b38efb3688a90fb
Isaac Ellon <155269775+IsaacEllon@users.noreply.github.com>
2025-04-22 15:51:33 -0300

    1

---
+++ 014-objeto-basico/objeto.js
@@ -0,0 +1,41 @@
+// const pessoa1 = {
+//     nome: 'Luiz',
+//     sobrenome: 'Miranda',
+//     idade: 25
+// };
+
+// const pessoa2 = {
+//     nome: 'Maria',
+//     sobrenome: 'Oliveira',
+//     idade: 55
+// };
+
+// console.log(pessoa1.nome);
+// console.log(pessoa1.sobrenome);
+
+
+// function criaPessoa(nome, sobrenome, idade) {
+//     return {
+//         nome,
+//         sobrenome,
+//         idade
+//     };
+// };
+
+// const pessoa1 = criaPessoa('Isaac', 'Ellon', 21);
+// const pessoa2 = criaPessoa('Luiz', 'Oliveira', 25);
+// const pessoa3 = criaPessoa('João', 'Miranda', 30);
+// const pessoa4 = criaPessoa('Héllen', 'Ferreira', 23);
+// const pessoa5 = criaPessoa('Jean', 'Otávio', 34);
+// console.log(pessoa1, pessoa2, pessoa3, pessoa4, pessoa5);
+
+
+const pessoa1 = {
+    nome: 'Luiz',
+    sobrenome: 'Miranda',
+    idade: 25
+
+    fala() {
+        console.log('Olá mundo');
+    }
+};

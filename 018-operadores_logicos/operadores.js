a1d02410ac6ef3acee62e4fd828a1fedf40d4ff8
Isaac Ellon <155269775+IsaacEllon@users.noreply.github.com>
2025-04-25 16:27:54 -0300

    1

---
+++ 018-operadores_logicos/operadores.js
@@ -0,0 +1,10 @@
+/*
+Operadores lógicos
+&& -> AND -> E  -> Todas as expressões precisam ser verdadeiras para retornar true.
+|| -> OR  -> OU -> No mínimo uma expressão precisa ser verdadeira para retornar true.
+!  -> NOT - NÃO -> Inverte a expressão
+*/
+
+console.log(true && true);
+console.log(true || false);
+console.log(!true);

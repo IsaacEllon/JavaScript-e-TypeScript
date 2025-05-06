d857896a077ed0d9c6bddf16ae35f4393d327ae1
Isaac Ellon <155269775+IsaacEllon@users.noreply.github.com>
2025-05-06 17:33:03 -0300

    1

---
+++ 027-for_classico/repeticaofor.js
@@ -0,0 +1,10 @@
+for (let i = 0; i <= 5; i++) {
+    const par = i % 2 === 0 ? 'par' : 'impar';
+    console.log(`linha ${i}, ${par}`);
+}
+
+const frutas = ['maça', 'Pera', 'Uva'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+}

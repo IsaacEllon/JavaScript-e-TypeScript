8d89a403afad48b81b0c7be8a3ba0d796cd36e9a
Isaac Ellon <155269775+IsaacEllon@users.noreply.github.com>
2025-05-07 17:09:54 -0300

    1

--- 029-for_of/forof.js
+++ 029-for_of/forof.js
@@ -1,6 +1,11 @@
 // Específico para estruturas iteraveis
-const nome = 'Isaac Ellon';
+// Não recomendado para objetos
+const nomes = ['Isaac', 'Ellon', 'Oliveira'];
 
-for (let valor of nome) {
+for (let valor of nomes) { // No lugar de retornar o índice, retorna o valor. 
     console.log(valor);
 };
+
+nomes.forEach(function(valor, indice, array) {
+    console.log(valor, indice, array);
+});
---
+++ 030-nodelist/assets/JS/main.js
@@ -0,0 +1,11 @@
+const paragrafos = document.querySelector('.paragrafos'); // Seleciona apenas a primeira estrutura que aparecer
+const pHtml = paragrafos.querySelectorAll('p'); // Seleciona todas as estrutudas solicitadas, no caso as tags <p> da div
+
+const estilosBody = getComputedStyle(document.body);
+const backgroundColorBody = estilosBody.backgroundColor;
+console.log(backgroundColorBody);
+
+for (let p of pHtml) {
+    p.style.backgroundColor = backgroundColorBody;
+    p.style.color = '#FFFFFF';
+}

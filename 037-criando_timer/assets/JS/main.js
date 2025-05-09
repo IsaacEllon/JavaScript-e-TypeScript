8d6829d6a64c7d6e50ca9c3c33eb7193bfd67aa1
Isaac Ellon <155269775+IsaacEllon@users.noreply.github.com>
2025-05-09 11:35:52 -0300

    1

--- 037-criando_timer/assets/JS/main.js
+++ 037-criando_timer/assets/JS/main.js
@@ -7,8 +7,8 @@ function criaHoraSegundos(segundos) {
 };
 
 const relogio = document.querySelector('.relogio');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
+// const iniciar = document.querySelector('.iniciar');
+// const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
 let segundos = 0;
 let timer;
@@ -20,26 +20,30 @@ function iniciaRelogio() {
     }, 1000);
 }
 
-document.addEventListener('click', function(e) {
-    const elemento = e.target;
+function cronometro () {
+    document.addEventListener('click', function(e) {
+    const elemento = e.target;  // target indica o que está sendo clicado
 
-    if (elemento.classList.contains('zerar')) {
+    if (elemento.classList.contains('zerar')) {  // contains serve para pegar a class
         zerar.addEventListener('click', function(event) {
         clearInterval(timer);
         relogio.innerHTML = '00:00:00';
-        segundos = 0;
-    });
+        segundos = 0;});
+        relogio.classList.remove('pausado');
     }
-})
 
-iniciar.addEventListener('click', function(event) {
-    relogio.classList.remove('pausado');
-    clearInterval(timer);
-    iniciaRelogio();
-});
+    if (elemento.classList.contains('iniciar')) {  // Se a class do objeto for iniciar, vai ser executado o bloco
+        relogio.classList.remove('pausado');
+        clearInterval(timer);
+        iniciaRelogio();
+    }
+    
+    if (elemento.classList.contains('pausar')) { // Se a class do objeto for pausar, vai ser executado o bloco
+        clearInterval(timer);
+        relogio.classList.add('pausado'); // adiciona a class do CSS .pausado.
+    }
+ });
 
-pausar.addEventListener('click', function(event) {
-    clearInterval(timer);
-    relogio.classList.add('pausado');
-});
+};
 
+cronometro();

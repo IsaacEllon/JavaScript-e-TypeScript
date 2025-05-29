4ca557360bbb82e55177e0ac2b0f8c69d06e22e2
Isaac Ellon <155269775+IsaacEllon@users.noreply.github.com>
2025-05-29 17:41:16 -0300

    1

--- 045-promises_ajax_fetch_async/03-ajax/assets/JS/xmlhttprequest.js
+++ 045-promises_ajax_fetch_async/03-ajax/assets/JS/xmlhttprequest.js
@@ -1,16 +1,19 @@
 // XMLHttpsRequest (GET) + Promises
 const request = obj => {
+    return new Promise ((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.open(obj.method, obj.url, true); // open(método, url, assincrono);
     xhr.send(null);
 
     xhr.addEventListener('load', () => {
         if(xhr.status >= 200 && xhr.status < 300) {
-            obj.success(xhr.responseText);
+            resolve(xhr.responseText);
         } else {
-            obj.error(xhr.statusText);
+            reject(xhr.statusText);
         }
     });
+    });
+    
 }
 
 document.addEventListener('click', e => {
@@ -23,7 +26,25 @@ document.addEventListener('click', e => {
     }
 })
 
-function carregaPagina(el) {
+async function carregaPagina(el) {
     const href = el.getAttribute('href');
     console.log(href);
+
+    const objConfig = {
+        method: 'GET',
+        url: href,
+    };
+
+    try {
+        const response = await request(objConfig);
+        carregaResultado(response)
+    } catch(e) {
+        console.log(e);
+    }
+    
+}
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = response;
 }

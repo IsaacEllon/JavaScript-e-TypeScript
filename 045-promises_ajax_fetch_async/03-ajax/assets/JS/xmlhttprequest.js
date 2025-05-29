1687d4008bee8705c4a074ca31e0115851d93a33
Isaac Ellon <155269775+IsaacEllon@users.noreply.github.com>
2025-05-29 16:17:52 -0300

    1

---
+++ 045-promises_ajax_fetch_async/03-ajax/assets/JS/xmlhttprequest.js
@@ -0,0 +1,29 @@
+// XMLHttpsRequest (GET) + Promises
+const request = obj => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(obj.method, obj.url, true); // open(método, url, assincrono);
+    xhr.send(null);
+
+    xhr.addEventListener('load', () => {
+        if(xhr.status >= 200 && xhr.status < 300) {
+            obj.success(xhr.responseText);
+        } else {
+            obj.error(xhr.statusText);
+        }
+    });
+}
+
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if(tag === 'a') {
+        e.preventDefault();
+        carregaPagina(el);
+    }
+})
+
+function carregaPagina(el) {
+    const href = el.getAttribute('href');
+    console.log(href);
+}

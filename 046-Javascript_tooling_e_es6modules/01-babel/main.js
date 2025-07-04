3341b97e7e164ccf35cfc4bd702c2fd118a5e9ba
Isaac Ellon <155269775+IsaacEllon@users.noreply.github.com>
2025-07-04 16:58:04 -0300

    1

---
+++ 046-Javascript_tooling_e_es6modules/01-babel/main.js
@@ -0,0 +1,10 @@
+class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+}
+// Comando para converter 
+//  npx babel main.js - o bundle.js --presets=@babel/env
+// npm run babel - para iniciar a conversão em tempo real
+// Ctrl + C - para parar 

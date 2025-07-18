814a680ce9d436bf6d9ec6fa86a9a1e3b4507f60
Isaac Ellon <155269775+IsaacEllon@users.noreply.github.com>
2025-07-18 12:17:23 -0300

    1

--- 046-Javascript_tooling_e_es6modules/03-gerador_cpf/webpack/src/main.js
+++ 046-Javascript_tooling_e_es6modules/03-gerador_cpf/webpack/src/main.js
@@ -1,1 +1,9 @@
+import GeraCPF from './modules/GeraCPF';
 import './assets/css/style.css';
+
+(function   () { // função autoexecutada
+    const gera = new GeraCPF();
+    const cpfGerado = document.querySelector('.cpf-gerado');
+    cpfGerado.innerHTML = gera.geraNovoCpf();
+
+})();

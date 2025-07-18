333830edf741084d282580f65d66930f48b0615f
Isaac Ellon <155269775+IsaacEllon@users.noreply.github.com>
2025-07-18 16:29:48 -0300

    1

--- 046-Javascript_tooling_e_es6modules/03-gerador_cpf/webpack/src/modules/GeraCPF.js
+++ 046-Javascript_tooling_e_es6modules/03-gerador_cpf/webpack/src/modules/GeraCPF.js
@@ -7,12 +7,12 @@ export default class GeraCPF {
     }
 
     formatado(cpf) {
-        return {
+        return (
             cpf.slice(0, 3) + '.' +
             cpf.slice(3, 6) + '.' +
             cpf.slice(6, 9) + '-' +
             cpf.slice(9, 11) 
-        }
+        )
     }
 
     geraNovoCpf() {
@@ -20,6 +20,6 @@ export default class GeraCPF {
         const digito1 = ValidaCPF.geraDigito(cpfSemDigitos);
         const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1);
         const novoCpf = cpfSemDigitos + digito1 + digito2;
-        return novoCpf;
+        return this.formatado(novoCpf);
     }
 }

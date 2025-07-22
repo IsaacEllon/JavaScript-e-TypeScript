1e6f49f0e794a0c28a1e5a0dfebdbab46fdefc66
Isaac Ellon <155269775+IsaacEllon@users.noreply.github.com>
2025-07-22 17:43:24 -0300

    1

---
+++ 047-node/02-modulos/app.js
@@ -0,0 +1,7 @@
+// const mod1 = require('./mod1');
+// const {nome, sobrenome, falaNome} = require('./mod1');
+// console.log(falaNome());
+
+const { Pessoa } = require('./mod1');
+const p1 = new Pessoa('Luiz');
+console.log(p1);

0903fd3abd6360270fcea704bdf5b4ccf241f5b4
Isaac Ellon <155269775+IsaacEllon@users.noreply.github.com>
2025-07-23 17:58:46 -0300

    1

--- 047-node/02-modulos/app.js
+++ 047-node/02-modulos/app.js
@@ -4,6 +4,8 @@
 // c1.latir();
 
 const path = require('path');
+console.log(path.resolve(__dirname, '..', '000-modelo', 'webpack'));
 
 console.log(__filename); // arquivo atual
 console.log(__dirname); // pasta atual
+
